4d1c7460787151ecf845e09fabeb4df1fa8f065d
SatyapriyaMahato <satyapriyamahto@gmail.com>
2023-09-10 12:39:25 +0530

    primisifying geolocation api

--- script.js
+++ script.js
@@ -38,6 +38,7 @@ const getJSON = function (url, errorMsg = 'Something went wrong') {
 };
 
 const apiLink = `https://countries-api-836d.onrender.com/countries`;
+const apiKey = `pk.18f82f578d8f656a2719c24a0974a158`;
 
 
 
@@ -219,7 +220,7 @@ const getCountryData = function (country) {
 // getCountryData('australia');
 
 // CHALLENEGE - 1
-// const apiKey = `pk.18f82f578d8f656a2719c24a0974a158`;
+
 
 
 
@@ -259,40 +260,40 @@ const getCountryData = function (country) {
 
 /****************************************************************/
 // Building a Simple Promise
-const lotteryPromise = new Promise(function (resolve, reject) {
-  console.log('Lotter draw is happening 🔮');
-  setTimeout(function () {
-    if (Math.random() >= 0.5) {
-      resolve('You WIN 💰');
-    } else {
-      reject(new Error('You lost your money 💩'));
-    }
-  }, 2000);
-});
+// const lotteryPromise = new Promise(function (resolve, reject) {
+//   console.log('Lotter draw is happening 🔮');
+//   setTimeout(function () {
+//     if (Math.random() >= 0.5) {
+//       resolve('You WIN 💰');
+//     } else {
+//       reject(new Error('You lost your money 💩'));
+//     }
+//   }, 2000);
+// });
 
-lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
+// lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
 
-// Promisifying setTimeout
-const wait = function (seconds) {
-  return new Promise(function (resolve) {
-    setTimeout(resolve, seconds * 1000);
-  });
-};
+// // Promisifying setTimeout
+// const wait = function (seconds) {
+//   return new Promise(function (resolve) {
+//     setTimeout(resolve, seconds * 1000);
+//   });
+// };
 
-wait(1)
-  .then(() => {
-    console.log('1 second passed');
-    return wait(1);
-  })
-  .then(() => {
-    console.log('2 second passed');
-    return wait(1);
-  })
-  .then(() => {
-    console.log('3 second passed');
-    return wait(1);
-  })
-  .then(() => console.log('4 second passed'));
+// wait(1)
+//   .then(() => {
+//     console.log('1 second passed');
+//     return wait(1);
+//   })
+//   .then(() => {
+//     console.log('2 second passed');
+//     return wait(1);
+//   })
+//   .then(() => {
+//     console.log('3 second passed');
+//     return wait(1);
+//   })
+//   .then(() => console.log('4 second passed'));
 
 // setTimeout(() => {
 //   console.log('1 second passed');
@@ -307,5 +308,40 @@ wait(1)
 //   }, 1000);
 // }, 1000);
 
-Promise.resolve('abc').then(x => console.log(x));
-Promise.reject(new Error('Problem!')).catch(x => console.error(x));
+// Promise.resolve('abc').then(x => console.log(x));
+// Promise.reject(new Error('Problem!')).catch(x => console.error(x));
+
+///////////////////////////////////////
+// Promisifying the Geolocation API
+const getPosition = function () {
+  return new Promise(function (resolve, reject) {
+    // navigator.geolocation.getCurrentPosition(
+    //   position => resolve(position),
+    //   err => reject(err)
+    // );
+    // here the position is automatically passed into the resolve as argument
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+};
+
+
+getPosition().then(pos => console.log(pos));
+
+
+const whereAmI = function () {
+  getPosition().then(pos => {
+    const { latitude: lat, longitude: lng } = pos.coords;
+    return fetch(`https://us1.locationiq.com/v1/reverse?key=${apiKey}&lat=${lat}&lon=${lng}&format=json`)
+  }).then((response) => {
+    if (!response.ok) throw new Error(`Something not right (${response.status})`);
+    return response.json();
+  })
+    .then(function (data) {
+      console.log(data);
+      getCountryData(data.address.country);
+    })
+  // .catch(Error => {
+  //   alert(Error) // Error!
+  // },);
+}
+btn.addEventListener('click', whereAmI);
